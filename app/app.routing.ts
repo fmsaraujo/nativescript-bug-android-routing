cdb4cb71378ecc00cb8cfa2b0b1843a8c4053e88
Fábio Manuel Serra Araújo <fabio.ms.araujo@gmail.com>
2017-07-03 21:46:49 +0100

    Code cleanup

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -14,9 +14,6 @@ const routes: Routes = [
       { path: '', component: ItemsComponent },
     ]
   }
-    // { path: "", redirectTo: "/items", pathMatch: "full" },
-    // { path: "items", component: ItemsComponent },
-    // { path: "item/:id", component: ItemDetailComponent },
 ];
 
 @NgModule({
